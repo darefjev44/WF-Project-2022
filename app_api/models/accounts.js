c34e7d27ad23b59ce903454d88904706ed00dea9
darefjev44 <daniel.arefjev@students.ittralee.ie>
2022-11-16 21:03:56 +0000

    Changed transactionSchema date field back to being of type Date, thought this may cause issues. Transactions list should be moved over to using Date objects again, just need to figure out a solution.

--- app_api/models/accounts.js
+++ app_api/models/accounts.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
     date: {
-        type: String,
+        type: Date,
         required: true
     },
     desc: {
